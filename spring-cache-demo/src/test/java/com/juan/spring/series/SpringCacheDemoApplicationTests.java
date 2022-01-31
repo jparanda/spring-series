5e3a3f165d4b3ee268f1994ddeeb780650ce3184
Juan Pablo Aranda Galvis <juan.aranda.galvis@gmail.com>
2022-01-31 06:47:54 -0600

    Init spring cache demo project

---
+++ spring-cache-demo/src/test/java/com/juan/spring/series/SpringCacheDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.spring.series;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCacheDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
