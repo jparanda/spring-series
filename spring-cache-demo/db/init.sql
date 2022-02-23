bc8c9d88fc37ebbeb75ac2a693b7accacb611451
Juan Pablo Aranda Galvis <juan.aranda.galvis@gmail.com>
2022-02-23 06:18:33 -0600

    Change script to insert initial data

--- spring-cache-demo/db/init.sql
+++ spring-cache-demo/db/init.sql
@@ -3,6 +3,7 @@ CREATE TABLE customer (
     first_name character varying(255) NOT NULL,
     last_name character varying(255) NOT NULL,
     dni character(225) NOT NULL,
+    age int not null,
     email character varying(255) NOT NULL,
     CONSTRAINT customer_dni_unique UNIQUE (dni)
 );
@@ -13,7 +14,7 @@ INCREMENT 1
 MINVALUE 1
 OWNED BY customer.id;
 
-INSERT INTO customer (id, first_name, last_name, dni, email) VALUES
-(nextval('customer_id_seq'), 'Juan Pablo', 'Aranda Galvis', '91111628' ,'juan.aranda.galvis@gmail.com'),
-(nextval('customer_id_seq'), 'Cristina Maria', 'Tellez Manrique', '37749843', 'tellez@gmail.com'),
-(nextval('customer_id_seq'), 'Maria Catalina', 'Aranda Tellez', '123456', 'catalina@gmail.com');
+INSERT INTO customer (id, first_name, last_name, dni, age, email) VALUES
+(nextval('customer_id_seq'), 'Juan Pablo', 'Aranda Galvis', '91111628' , 39, 'juan.aranda.galvis@gmail.com'),
+(nextval('customer_id_seq'), 'Cristina Maria', 'Tellez Manrique', '37749843', 40, 'tellez@gmail.com'),
+(nextval('customer_id_seq'), 'Maria Catalina', 'Aranda Tellez', '123456', 10, 'catalina@gmail.com');
